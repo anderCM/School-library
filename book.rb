bda54850b4f5a07e9be3a227ec5babcbec87a9e2
anderCM <andercm15@gmail.com>
2023-04-20 16:27:26 -0500

    has-many and belongs-to relations for Book and Rental classes created

--- book.rb
+++ book.rb
@@ -1,8 +1,15 @@
 class Book
   attr_accessor :tittle, :author
+  attr_reader :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
+  end
+
+  def add_rental(rental)
+    @rentals << rental
+    rental.book = self
   end
 end
--- rental.rb
+++ rental.rb
@@ -1,7 +1,12 @@
 class Rental
-  attr_accessor :date
+  attr_accessor :date, :person
 
   def initialize(date)
     @date = date
+    @person = person
+  end
+
+  def add_book(book)
+    book.add_rental(self)
   end
 end
