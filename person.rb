ed0baa120057e2e0c6cea3966dadfcf0d909ccd2
anderCM <andercm15@gmail.com>
2023-04-20 12:52:35 -0500

    Classroom class created

--- person.rb
+++ person.rb
@@ -28,10 +28,3 @@ class Person < Nameable
     @age >= 18 || @parent_permission
   end
 end
-
-person = Person.new('maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
