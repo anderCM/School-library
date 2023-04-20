099f72fe1683ef5cc262503da55386718bb274a1
anderCM <andercm15@gmail.com>
2023-04-20 18:14:00 -0500

    Setter for rentals created

--- person.rb
+++ person.rb
@@ -5,8 +5,8 @@ require_relative 'book'
 require_relative 'rental'
 
 class Person < Nameable
-  attr_reader :id, :rentals
-  attr_accessor :name, :age
+  attr_reader :id
+  attr_accessor :name, :age, :rentals
 
   def initialize(name, age = 'Unknown', permission: true)
     super()
